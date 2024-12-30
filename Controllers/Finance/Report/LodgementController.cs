6ffd73bc1b745ae404937e4d7bae222b42fe45d7
waqas <waqasriasat1989@gmail.com>
2024-12-30 18:10:53 +0500

    Update 30-12-2024 O

--- Controllers/Finance/Report/LodgementController.cs
+++ Controllers/Finance/Report/LodgementController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Exampler_ERP.Controllers.Setup;
+using Exampler_ERP.Models;
+using Exampler_ERP.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Exampler_ERP.Controllers.Finance.Report
 {
   public class LodgementController : Controller
   {
-    public IActionResult Index()
+    private readonly AppDBContext _appDBContext;
+    private readonly IConfiguration _configuration;
+    private readonly Utils _utils;
+    private readonly ILogger<HRGlobalSettingController> _logger;
+    public LodgementController(AppDBContext appDBContext, IConfiguration configuration, Utils utils, ILogger<HRGlobalSettingController> logger)
     {
-      return View();
+      _appDBContext = appDBContext;
+      _configuration = configuration;
+      _utils = utils;
+      _logger = logger;
     }
+    public async Task<IActionResult> Index()
+    {
+      // Fetching the list dynamically
+      ViewBag.HeadofAccount_FiveList = await _utils.GetHeadofAccount_FiveOnlyCashandBank();
+
+      // Returning the Lodgement view dynamically with the appropriate model
+      return View("~/Views/Finance/Report/Lodgement/Lodgement.cshtml", new FI_Voucher());
+    }
+
   }
 }
